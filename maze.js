5a50fe3776271a51d5102599e6369c8afd88279c
Sam Berg <stberg3@gmail.com>
2023-04-07 08:12:40 -0500

    Refactoring to make things ez, or to procrastinate

--- maze.js
+++ maze.js
@@ -1,55 +1,43 @@
-
-function showBox() {
-	var grid = new Array(5)
-	
-	for(i=0; i<5; i++) {
-		grid[i] = new Array(5)
-		
-		for(j=0; j<5; j++) {
-			grid[i][j] = new GridBox(i,j,true, true, true, true);
-		}		
+class Grid {
+	constructor(rows, cols) {
+		this.grid = new Array(rows);
+		this.rows = rows;
+		this.cols = cols;
+		this.render();
 	}
 
-	backtrack(grid[0][1], grid);
-}
+	render() {	
+		for(let i=0; i<this.rows; i++) {
+			this.grid[i] = new Array(5)
+			
+			for(let j=0; j<this.cols; j++) {
+				this.grid[i][j] = new GridBox(i, j, this.grid);
+			}		
+		}
+	}
+
+	makeMaze() {		
+		backtrack(grid[0][1], grid);
+	}
 
 
-function backtrack(box,grid) {	
-	console.log(`[${box.col},${box.row}]`)
+	backtrack(box, grid) {	
+		console.log(`[${box.col},${box.row}]`)
 
-	grid[box.row][box.col].visited = true;
-	grid[box.row][box.col].render();
+		grid[box.row][box.col].visited = true;
+		grid[box.row][box.col].render();
 
-	let offsets = [[0,1],[0,-1],[1,0],[-1,0]];
-	let neighbors = offsets
-		.map((offset) => [box.col+offset[0], box.row+offset[1]]);
-	neighbors = neighbors.filter((newCoord) => {
-			let okay = true;
-			let newCol = newCoord[1];
-			let newRow = newCoord[0];
+		let offsets = [[0,1],[0,-1],[1,0],[-1,0]];
+		let neighbors = grid[box.row][box.col].neighbors()
 
-			okay &&= newRow >= 0 && newRow < grid[0].length			
-			okay &&= newCol >= 0 && newCol < grid[1].length
+		if(neighbors.length == 0) return;
 
-			return okay && !grid[newCol,newRow].visited;
-		});
-	neighbors = neighbors.map((newCoord) => {
-			let newRow = newCoord[0];
-			let newCol = newCoord[1];
-			return grid[newRow][newCol];
-		});
-
-	console.log(`BOX ${box}: (${neighbors.length}) ` + neighbors.join(';'))
-	if(neighbors.length == 0) return;
-
-	let neighbor = neighbors[Math.floor(Math.random(1)*neighbors.length)]
-	console.log(`Joining ${box} to ${neighbor}`)
-	console.log(`\t BOX ${box.row} ${box.col}`)
-	console.log(`\t NEIGHBOR ${neighbor} ${neighbor.row} ${neighbor.col}`)
-	box.join(neighbor);
-	backtrack(neighbor, grid);
-	
-	return
+		let neighbor = neighbors[Math.floor(Math.random(1)*neighbors.length)]
+		box.join(neighbor);
+		backtrack(neighbor, grid);
+		
+		return;
+	}
 }
 
 class GridBox {
@@ -58,16 +46,39 @@ class GridBox {
 	g = 10;
 	visited = false;
 
-	constructor(row,col,l,t,r,b) {
-		this.l = l;
-		this.t = t;
-		this.r = r;
-		this.b = b;
+	constructor(row,col,grid) {
+		this.l = true;
+		this.t = true;
+		this.r = true;
+		this.b = true;
 		this.row = row;
 		this.col = col;
+		this.grid = grid;
 		this.render();
 	}
 	
+	neighbors() {
+		let offsets = [[0,1],[0,-1],[1,0],[-1,0]];
+		let neighbors = offsets
+			.map((offset) => [this.col+offset[0], this.row+offset[1]])
+			.filter((newCoord) => {
+				let isValid = true;
+				let newCol = newCoord[1];
+				let newRow = newCoord[0];
+
+				isValid &&= 0 <= newRow && newRow < this.grid[0].length;		
+				isValid &&= 0 <= newCol && newCol < this.grid[1].length;
+
+				return isValid && !this.grid[newCol,newRow].visited;
+			}).map((newCoord) => {
+				let newRow = newCoord[0];
+				let newCol = newCoord[1];
+				return this.grid[newRow][newCol];
+			});
+
+		return neighbors;
+	}
+
 	render() {
 		const canvas = document.querySelector('canvas');
 		const ctx = canvas.getContext('2d');
@@ -100,8 +111,6 @@ class GridBox {
 	}
 
 	join(neighbor) {
-		console.log(`We're joining ${neighbor} with ${this}`);
-		console.log(`\t ${this.col}||${neighbor.col} ${this.row}||${neighbor.row}`);
 
 		if(this.col < neighbor.col) {
 			neighbor.l = false;
