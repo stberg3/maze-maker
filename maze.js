d3300dfea2cfa0700d78d3b4ceb334954cddf1d1
Sam Berg <stberg3@gmail.com>
2023-04-09 21:40:59 -0500

    It's now animated!

--- maze.js
+++ maze.js
@@ -3,6 +3,8 @@ const SEARCHED = 1;
 const START = 2;
 const END = 3;
 const PATH = 4;
+const NEW = 5;
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
 class Grid {
 	constructor(rows, cols) {
@@ -45,11 +47,15 @@ class Grid {
 		}
 	}
 
-	solveMaze() {
+	
+
+	async solveMaze() {
 		let searched = new Set()
 		let q = [this.grid[0][0]];
 
 		while(q.length > 0) {
+			await delay(25);
+
 			let probe = q.shift();
 			probe.status = SEARCHED;
 			probe.render();
@@ -61,7 +67,7 @@ class Grid {
 				probe.status = END;
 				probe.render();
 				console.log("We found the end!");
-				return probe;
+				return Promise.resolve(probe);
 			}
 
 			let neighbors = this.graph.get(probe.getKey())
@@ -71,11 +77,13 @@ class Grid {
 				q.push(n);
 			});
 		}
+		
 
 		console.error("I couldn't find the end 😖")
+		return Promise.resolve(null);
 	}
 
-	iterativeDfs() {
+	async iterativeDfs() {
 		let box = this.grid[0][0];
 		let stack = new Array();
 		box.status = true;
@@ -83,6 +91,7 @@ class Grid {
 		stack.push(box);
 
 		while(stack.length > 0) {
+			await delay(25);
 			let curr = stack.pop();
 			let neighbors = curr.neighbors(VISITED)
 				.filter((n) => !this.visited[n.row][n.col]);
@@ -99,6 +108,8 @@ class Grid {
 				this.graph.get(neighbor.getKey()).push(curr);
 			}
 		}
+
+		return Promise.resolve(null)
 	}
 
 	backtrack(box) {
@@ -129,7 +140,7 @@ class GridBox {
 	w = 20;
 	h = 20;
 	g = 10;
-	status = false;
+	status = NEW;
 	westWall = true;
 	northWall = true;
 	eastWall = true;
@@ -186,8 +197,11 @@ class GridBox {
 			case(PATH):
 				ctx.fillStyle = "#FFFF55";
 				break;
+			case(NEW):
+				ctx.fillStyle = "#4F4F4F";
+				break;
 			default:
-				ctx.fillStyle = "#FF3333";
+				ctx.fillStyle = "#FF3F3F";
 		}		
 
 		ctx.fillRect(this.w*this.col, this.h*this.row, this.w, this.h);
@@ -195,8 +209,6 @@ class GridBox {
 		ctx.lineWidth = 1;
 
 		if(this.westWall) {
-			ctx.strokeStyle = 'black';
-			// ctx.fillRect(this.h*this.col, this.w*this.row, this.g/4, this.h);
 			ctx.beginPath();
 			ctx.moveTo(this.w*this.col, this.h*this.row);
 			ctx.lineTo(this.w*this.col, this.h*(this.row+1));
@@ -204,7 +216,6 @@ class GridBox {
 		}
 
 		if(this.northWall) {
-			ctx.strokeStyle = 'black';
 			ctx.beginPath();
 			ctx.moveTo(this.w*this.col, this.h*this.row);
 			ctx.lineTo(this.w*(this.col+1), this.h*this.row);
@@ -212,7 +223,6 @@ class GridBox {
 		}
 
 		if(this.eastWall) {
-			ctx.strokeStyle = 'black';
 			ctx.beginPath();
 			ctx.moveTo(this.w*(this.col+1), this.h*this.row);
 			ctx.lineTo(this.w*(this.col+1), this.h*(this.row+1));
@@ -220,7 +230,6 @@ class GridBox {
 		}
 
 		if(this.southWall) {
-			ctx.strokeStyle = 'black';
 			ctx.beginPath();
 			ctx.moveTo(this.w*this.col, this.h*(this.row+1));
 			ctx.lineTo(this.w*(this.col+1), this.h*(this.row+1));
@@ -262,12 +271,7 @@ class GridBox {
 	}
 
 	toString() {
-		return `[${this.row},${this.col}] `  
-				 // `${this.visited ? 'visited' : 'unvisited'} ` +
-				 // `N: ${this.northWall ? 'X' : 'O'} ` + 
-				 // `E: ${this.eastWall ? 'X' : 'O'} ` + 
-				 // `S: ${this.southWall ? 'X' : 'O'} ` +
-				 // `W: ${this.northWall ? 'X' : 'O'}`
+		return `[${this.row},${this.col}] `
 	}
 
 }
